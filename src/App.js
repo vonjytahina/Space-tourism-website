e835fa00f2d82c37720f9204a4c4bfa375de1cb4
vonjytahina <vonjytahinag@gmail.com>
2023-07-14 12:08:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Technology from "./components/Technology";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter> 
       <>
         <Routes>
           <Route path="/" element={<Home />} />       
